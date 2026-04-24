88e08b69ca7b35646846a23fdb00b4e8fd06df40
marikm <matsumotomarina3@gmail.com>
2026-04-24 19:34:49 -0300

    project created

---
+++ src/main/java/com/matsumoto/encanto/EncantoApplication.java
@@ -0,0 +1,13 @@
+package com.matsumoto.encanto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncantoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncantoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matsumoto/encanto/EncantoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matsumoto.encanto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncantoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
