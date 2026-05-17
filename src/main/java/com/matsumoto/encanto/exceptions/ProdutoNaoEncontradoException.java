5ee4689a3972170b6cc766e268281dc8de4dd524
marikm <matsumotomarina3@gmail.com>
2026-05-16 21:32:50 -0300

    feat(produto): add ProdutoNaoEncontradoException and ProdutoRepository

---
+++ src/main/java/com/matsumoto/encanto/exceptions/ProdutoNaoEncontradoException.java
@@ -0,0 +1,5 @@
+package com.matsumoto.encanto.exceptions;
+
+public class ProdutoNaoEncontradoException extends RuntimeException{
+    public ProdutoNaoEncontradoException(String message){ super(message);}
+}
