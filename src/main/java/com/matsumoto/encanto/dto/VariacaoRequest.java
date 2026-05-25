0e7da69c39ababb72d22b540eabb094af600a26f
marikm <matsumotomarina3@gmail.com>
2026-05-24 22:42:12 -0300

    feat: add VariacaoRequest and VariacaoResponse DTOs

--- src/main/java/com/matsumoto/encanto/dto/VariacaoRequest.java
+++ src/main/java/com/matsumoto/encanto/dto/VariacaoRequest.java
@@ -1,4 +1,26 @@
 package com.matsumoto.encanto.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class VariacaoRequest {
+    private String cor;
+    private String tamanho;
+    @Positive
+    private Double peso;
+    @Positive
+    @NotNull
+    private Double preco;
+    @Positive
+    private Integer prazoEmDias;
+    private String foto;
+
 }
