9fd97db7a9465ad43f1a377c16ae25ff73c8f61f
marikm <matsumotomarina3@gmail.com>
2026-05-24 22:41:47 -0300

    feat: add VariacaoRepository

--- src/main/java/com/matsumoto/encanto/repository/VariacaoRepository.java
+++ src/main/java/com/matsumoto/encanto/repository/VariacaoRepository.java
@@ -1,4 +1,12 @@
 package com.matsumoto.encanto.repository;
 
-public interface VariacaoRepository {
+import com.matsumoto.encanto.domain.Variacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface VariacaoRepository extends JpaRepository<Variacao, Integer> {
+
+    List<Variacao> findByProdutoId(Integer produtoId);
+
 }
