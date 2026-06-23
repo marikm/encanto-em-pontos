e6d1ace4c617541c9e6a8df85b7dbc0754cddf80
marikm <matsumotomarina3@gmail.com>
2026-06-23 19:47:19 -0300

    feat: add JwtService

---
+++ src/main/java/com/matsumoto/encanto/service/JwtService.java
@@ -0,0 +1,53 @@
+package com.matsumoto.encanto.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY = "${SECRET_KEY}";
+    private static final long EXPIRACAO_MS = 86400000; // 24h
+
+    public String gerarToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .claim("perfil",
+                        userDetails.getAuthorities().iterator().next().getAuthority())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() +
+                        EXPIRACAO_MS))
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+    public String extrairEmail(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    private boolean isTokenExpirado(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean isTokenValido(String token, UserDetails userDetails) {
+        String email = extrairEmail(token);
+        return email.equals(userDetails.getUsername()) && !isTokenExpirado(token);
+    }
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
+
