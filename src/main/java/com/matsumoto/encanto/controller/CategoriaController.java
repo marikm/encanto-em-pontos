9e09c4d9eae09162b764a3668baf0f791380c42b
marikm <matsumotomarina3@gmail.com>
2026-06-27 21:25:19 -0300

    feat: add CategoriaController with GET and POST endpoints

--- src/main/java/com/matsumoto/encanto/controller/CategoriaController.java
+++ src/main/java/com/matsumoto/encanto/controller/CategoriaController.java
@@ -1,13 +1,39 @@
 package com.matsumoto.encanto.controller;
 
-import com.matsumoto.encanto.domain.Categoria;
+import com.matsumoto.encanto.dto.CategoriaRequest;
+import com.matsumoto.encanto.dto.CategoriaResponse;
+import com.matsumoto.encanto.service.CategoriaService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/categorias")
 public class CategoriaController {
+
+    private final CategoriaService categoriaService;
+
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaResponse>> listarCateogias() {
+        List<CategoriaResponse> categorias = categoriaService.listarTodos();
+        return ResponseEntity.ok().body(categorias);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaResponse> buscarCategoriaPorId(@PathVariable Integer id) {
+        return ResponseEntity.ok(categoriaService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoriaResponse> criarCategoria(@RequestBody @Valid CategoriaRequest categoriaRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoriaService.criar(categoriaRequest));
+    }
 
 }
