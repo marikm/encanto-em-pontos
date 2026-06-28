1e10fd3ed747e8f31e92a95ee471eaa54526262a
marikm <matsumotomarina3@gmail.com>
2026-06-27 21:24:08 -0300

    feat: add CategoriaResponse DTO

---
+++ src/main/java/com/matsumoto/encanto/dto/CategoriaResponse.java
@@ -0,0 +1,16 @@
+package com.matsumoto.encanto.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoriaResponse {
+
+    private Integer id;
+    private String nome;
+}
